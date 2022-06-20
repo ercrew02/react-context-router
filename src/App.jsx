f0567ef916c742ce68839715161dc20114c6726a
Muhammad Vikri <muhammadvikrii99@gmail.com>
2022-06-20 14:31:49 +0700

    delete Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+
 import './App.css';
 import { ContextProvider } from './context/context';
 
 import AppRoutes from './routes/AppRoutes';
 
-import { store } from './store/Store';
-
 function App() {
   return (
     <ContextProvider>
-      <Provider store={store}>
-        <AppRoutes />
-      </Provider>
+      <AppRoutes />
     </ContextProvider>
   );
 }
